d28364bfbbfea4f148fd2c4872700ec91cbff6da
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-05-05 21:45:54 +0200

    Updates

---
+++ precache-manifest.92f9322c83c27587b7d403e9d57de336.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b7dd2b6c16c2b7887ae945855ce2f6",
+    "url": "/lucky-react-starter/index.html"
+  },
+  {
+    "revision": "d144b4c90e2b3e0ab66b",
+    "url": "/lucky-react-starter/static/js/2.c201c964.chunk.js"
+  },
+  {
+    "revision": "7a7358e49a61341cb9ea",
+    "url": "/lucky-react-starter/static/js/main.beba1ecf.chunk.js"
+  },
+  {
+    "revision": "2120748c51b0ca8926de",
+    "url": "/lucky-react-starter/static/js/runtime~main.4b49b22b.js"
+  }
+]);
