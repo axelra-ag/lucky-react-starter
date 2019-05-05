a75e4e338d719f2f893d255ff5bdfc9b975ecdc5
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-05-05 17:27:00 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, MainTheme } from "./layout/Theme";
+import store from "./reducers/store";
 
 // Entry Point of your App
 // Have Fun By https://github.com/lucaspelloni2 :-)
@@ -13,7 +15,9 @@ const MyApp = () => {
       <ThemeProvider theme={MainTheme}>
         <GlobalStyle />
       </ThemeProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Fragment>
   );
 };
