4bc520599339db9c341d5453f8ecbfa1fd28cb72
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-05-11 14:23:23 +0200

    Update Theme.ts

--- src/layout/Theme.ts
+++ src/layout/Theme.ts
@@ -1,56 +1,56 @@
-import { CSSProperties } from "react";
-import { createGlobalStyle, ThemeProviderProps } from "styled-components";
+import {CSSProperties} from "react";
+import {createGlobalStyle, ThemeProviderProps} from "styled-components";
 
 type Props = {
-  googleUrl: string;
+    googleUrl: string;
 };
 export const MainTheme: CSSProperties & Props = {
-  fontFamily: "Gothic A1, sans-serif",
-  googleUrl: "https://fonts.googleapis.com/css?family=Gothic+A1:200,400,700",
-  boxShadow: "0 4px 6px rgba(50,50,93,0.11), 0 1px 3px rgba(0,0,0,0.08)"
-  // add more general CSS properties here
+    fontFamily: "Gothic A1, sans-serif",
+    googleUrl: "https://fonts.googleapis.com/css?family=Gothic+A1:200,400,700",
+    boxShadow: "0 4px 6px rgba(50,50,93,0.11), 0 1px 3px rgba(0,0,0,0.08)"
+    // add more general CSS properties here
 };
 
 // Colors Palette https://colorhunt.co/
 export enum __COLORS {
-  PRIMARY = "#f38181",
-  SECONDARY = "#fce38a",
-  TERTRIARY = "#eaffd0",
-  FOURTH = "#95e1d3",
-  WHITE = "#fff",
-  BLACK = "#000"
+    PRIMARY = "#f38181",
+    SECONDARY = "#fce38a",
+    TERTRIARY = "#eaffd0",
+    FOURTH = "#95e1d3",
+    WHITE = "#fff",
+    BLACK = "#000"
 }
 export enum __GRAY_SCALE {
-  _WHITE = "#fff",
-  _100 = "#f6f9fc",
-  _200 = "#e9ecef",
-  _300 = "#dee2e6",
-  _400 = "#ced4da",
-  _500 = "#adb5bd",
-  _600 = "#8898aa",
-  _700 = "#525f7f",
-  _800 = "#32325d",
-  _900 = "#212529",
-  _BLACK = "#000"
+    _WHITE = "#fff",
+    _100 = "#f6f9fc",
+    _200 = "#e9ecef",
+    _300 = "#dee2e6",
+    _400 = "#ced4da",
+    _500 = "#adb5bd",
+    _600 = "#8898aa",
+    _700 = "#525f7f",
+    _800 = "#32325d",
+    _900 = "#212529",
+    _BLACK = "#000"
 }
 
 export enum __ALERTS {
-  INFO = "#11cdef",
-  SUCCESS = "#2ece89",
-  WARNING = "#fb6240",
-  ERROR = "#f5365c"
+    INFO = "#11cdef",
+    SUCCESS = "#2ece89",
+    WARNING = "#fb6240",
+    ERROR = "#f5365c"
 }
 // You can either import a Google Font: https://fonts.google.com
 // Or declare a custom font: https://tinyurl.com/y6omstqa
 // eslint-disable-next-line no-unexpected-multiline
 export const GlobalStyle = createGlobalStyle<{
-  theme: ThemeProviderProps<any>;
+    theme: ThemeProviderProps<any>;
 }>`
   @import url(${MainTheme.googleUrl});
   html {
     font-family: ${(p: ThemeProviderProps<any>) => p.theme.fontFamily};
   }
-  body {
+    body {
     padding: 0;
     margin: 0;
     border: 0;
@@ -65,6 +65,7 @@ export const GlobalStyle = createGlobalStyle<{
     color: ${__COLORS.PRIMARY};
     font-weight: 100;
     line-height: 1.4;
+     font-family: inherit;
   }
   
   h1 {
@@ -74,5 +75,18 @@ export const GlobalStyle = createGlobalStyle<{
   p {
     word-break: break-word;
     line-height: 1.4;
+     font-family: inherit;
+  }
+  input, textarea {
+  box-shadow: none;
+   font-family: inherit;
+    -webkit-appearance: none;
+    border-width: initial;
+    border-style: none;
+    border-color: initial;
+    border-image: initial;
+    border-radius: 0;
+    margin: 0;
+    padding: 0;
   }
 `;
