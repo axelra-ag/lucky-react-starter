bd144af142559f7b1fcceaafcd69aa4fe3e5ce6b
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-09-08 14:27:41 +0200

    icon defined

--- src/components/Documentation.tsx
+++ src/components/Documentation.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 import {Container} from "../UI/layout/Container";
+import {__COLORS, SPACING} from "../UI/theme/Theme";
+import {Icon, IconTypes} from "./views/Icon";
+
+const MyIcon = styled(Icon)``;
 
 export const Documentation = () => {
-  return <Container>Docs</Container>;
+  return (
+    <Container>
+      <MyIcon name={IconTypes.PICTURE} color={__COLORS.CTA} />
+    </Container>
+  );
 };
