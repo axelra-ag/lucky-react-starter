89e9106013d71a16b1465b930c2416bd75f3bd9c
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-09-08 20:58:54 +0200

    Update Container.ts

--- src/UI/layout/grid/Container.ts
+++ src/UI/layout/grid/Container.ts
@@ -12,19 +12,19 @@ import {
 // ${props => mediaQueries("md")(`width: ${props.width}px`)}
 const media = css`
   ${makeQuery(__MEDIA_QUERY_BREAK_POINT.SMALL)(`
-      width: ${SMALL_DEVICES_BREAK_POINT}px
+      max-width: ${SMALL_DEVICES_BREAK_POINT}px
   `)};
 
   ${makeQuery(__MEDIA_QUERY_BREAK_POINT.MEDIUM)(`
-      width: ${MEDIUM_DEVICES_BREAK_POINT}px;
+      max-width: ${MEDIUM_DEVICES_BREAK_POINT}px;
   `)};
 
   ${makeQuery(__MEDIA_QUERY_BREAK_POINT.LARGE)(`
-      width: ${LARGE_DEVICES_BREAK_POINT}px
+      max-width: ${LARGE_DEVICES_BREAK_POINT}px
   `)};
 
   ${makeQuery(__MEDIA_QUERY_BREAK_POINT.EXTRA_LARGE)(`
-      width: ${EXTRA_LARGE_DEVICES_BREAK_POINT}px
+      max-width: ${EXTRA_LARGE_DEVICES_BREAK_POINT}px
   `)};
 `;
 
