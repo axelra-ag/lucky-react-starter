ed7f7fe2942b8314e91b1a5728b80e4c424b835f
lucaspelloni2 <lucas_pelloni@hotmail.com>
2020-09-14 13:38:14 +0200

    Update actions.tsx

--- src/reducers/todos/actions.tsx
+++ src/reducers/todos/actions.tsx
@@ -1,14 +1,11 @@
 import {ThunkDispatch} from "redux-thunk";
 import {fetch as fetchPolyfill} from "whatwg-fetch";
-import {getDomain} from "../../helpers/get-domain";
-import {PROTOCOL_METHOD} from "../../helpers/get-fetch-options";
 import {
-  ActionTypes,
-  ErrorTodos,
-  FetchedTodos,
-  LoadingTodos,
-  Todo
-} from "./types";
+  createRequest,
+  HTTP_REQUEST_METHODS
+} from "../../helpers/create-request";
+import {baseURL} from "../../api";
+import {ActionTypes, ErrorTodos, FetchedTodos, LoadingTodos} from "./types";
 
 export const fetchTodos = (): any => {
   return async (
@@ -19,21 +16,24 @@ export const fetchTodos = (): any => {
       loading: true
     });
 
-    /*    fetchPolyfill(`${getDomain()}/todos`, HTTP_OPTIONS(PROTOCOL_METHOD.GET))
-      .then(res => res.json())
-      .then((todos: Todo[]) => {
-        dispatch({
-          type: ActionTypes.FETCHED_TODOS,
-          loading: false,
-          todos
-        });
-      })
-      .catch((error: string) => {
-        dispatch({
-          type: ActionTypes.ERROR_TODOS,
-          error,
-          loading: false
-        });
-      });*/
+    const response = await fetchPolyfill(
+      `${baseURL}/todos`,
+      createRequest({method: HTTP_REQUEST_METHODS.GET})
+    );
+    const json = await response.json();
+    const {error, todos} = json;
+    if (!json.success) {
+      dispatch({
+        type: ActionTypes.ERROR_TODOS,
+        error,
+        loading: false
+      });
+    } else {
+      dispatch({
+        type: ActionTypes.FETCHED_TODOS,
+        loading: false,
+        todos
+      });
+    }
   };
 };
